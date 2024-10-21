0e474d75246fc7e66bc8ed8f8e93c76ed1d05c5e
Tynashe2853U <168686441+Tynashe2853U@users.noreply.github.com>
2024-10-21 16:53:32 +0200

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        public static void FizzBuzz()
+        {
+            int num = 1;
+            while (num <= 100)
+            {
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (num % 5 == 0 ) {
+
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+                num++;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            FizzBuzz();
+        }
+    }
+}
